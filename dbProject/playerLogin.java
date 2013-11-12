4b5117b3d5c466934db5f02ba6ef2f98d9ad3dbc
sagar <sjsagarjha3@gmail.com>
2013-11-12 16:01:47 +0530

    added the functionality of seeing club news in player profile

--- dbProject/playerLogin.java
+++ dbProject/playerLogin.java
@@ -133,89 +133,38 @@ class playerLogin extends HttpServlet{
             }
         }
 
-	// More Club and More News
+	// Club News
 	{
-	    String query = "select clubid from member where playerid="+id+";";
+	    String query = "select description from member, news where member.playerid="+id+" and member.clubid=news.clubid;";
 	    System.out.println(query);
 	    rs = stmt.executeQuery (query);
-	    String news = "";
-            String record_news="";
-            int clubid=0;
-            int club1id=0;
-            int club2id=0;
-            int club3id=0;
-            String club=null;
-            String club1=null;
-            String club2=null;
-            String club3=null;
             String news=null;
             String news1=null;
             String news2=null;
             String news3=null;
-            
+
 	    int count = 1;
-            int count2=1;    //for news
-            String clubName="";
-            String clubNews="";
+	    if (rs.next()) {
+		request.setAttribute("news1",rs.getString("description"));
+		news += count + ". " + rs.getString("description") + "\\n";
+		count++;
+	    }
+	    if (rs.next()) {
+		request.setAttribute("news2",rs.getString("description"));
+		news += count + ". " + rs.getString("description") + "\\n";
+		count++;
+	    }
+	    if (rs.next()) {
+		request.setAttribute("news3",rs.getString("description"));
+		news += count + ". " + rs.getString("description") + "\\n";
+		count++;
+	    }
 	    while (rs.next()) {
-                clubid=rs.getInt("clubid");
-                clubName="select name from club where id="+clubid+";";
-                clubNews="select description from news where clubid="+clubid+";";
-                ResultSet rs2=stmt2.executeQuery(clubName);
-                ResultSet rs3=stmt3.executeQuery(clubNews);
-                if(rs2.next())
-		    {
-			//Club Name Part
-			club=rs2.getString("name");
-			if(count==1)
-			    {
-				club1id=rs.getInt("clubid");
-				club1=rs2.getString("name");
-				request.setAttribute("club1",club1);
-			    }
-			if(count==2)
-			    {
-				club2id=rs.getInt("clubid");
-				club2=rs2.getString("name");
-				request.setAttribute("club2",club2);
-			    }
-			if(count==3)
-			    {
-				club3id=rs.getInt("clubid");
-				club3=rs2.getString("name");
-				request.setAttribute("club3",club3);
-			    }
-			news += count + ". " + club + "\\n";
-			count++;
-		    }
-                
-                //News for each club
-                while(rs3.next())
-		    {
-			news=rs3.getString("description");
-			if(count2==1)
-			    {
-				news1=rs3.getString("description");
-				request.setAttribute("news1",news1);
-			    }
-			if(count2==2)
-			    {
-				news2=rs3.getString("description");
-				request.setAttribute("news2",news2);
-			    }
-			if(count2==3)
-			    {
-				news3=rs3.getString("description");
-				request.setAttribute("news3",news3);
-			    }
-			record_news+= count2+". "+ news + "\\n";
-			count2++;
-		    }
-                
+		news += count + ". " + rs.getString("description") + "\\n";
+		count++;
 	    }
 	    System.out.println(news);
-            request.setAttribute("morenews",record_news);
-	    request.setAttribute("moreclub",news);
+	    request.setAttribute("news",news);
 	}
     }
 }
