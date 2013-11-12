18d49ae73e21f8b028f9a99082a50e6a3efb97ca
Rogyan <rohan@ubuntu.ubuntu-domain>
2013-11-12 22:21:31 +0530

    To make make work

--- tennis.java
+++ tennis.java
@@ -277,14 +277,43 @@ public class tennis extends HttpServlet{
                         new_id=rs.getInt("idvalue");
                     conn.setAutoCommit(false);
                     query="Update Constant set IDValue = IDValue+1 where ConstantName = ‘account’;";
-                    
-                    conn.setAutoCommit(true);
+                    stmt.executeUpdate(query);
+                    int yearofbirth=0;
+                    query="Insert into Player(ID,Name, Username, Password,  Address, Description,PhoneNum,EmailID, YeaOfBirth, Gender, RoleModel) values ("+new_id+", '"+session.getAttribute("name")+"', '"+session.getAttribute("username")+"', '"+session.getAttribute("password")+"','" +session.getAttribute("address")+"', '"+session.getAttribute("description")+"', '"+session.getAttribute("phoneno")+"', '"+session.getAttribute("emailaddress")+"', "+yearofbirth+", '"+session.getAttribute("gender")+"', '"+session.getAttribute("rolemodel")+"');";
+                    stmt.executeUpdate(query);
+                    query="Insert into member(playerid, clubid) values ("+new_id+", "+session.getAttribute("clubid")+");";
+                    stmt.executeUpdate(query);
+                    conn.commit();
                 }
-                catch(Exception e)
+                catch(SQLException e)
                 {
                     System.out.println(e);
+                    if (conn != null) 
+                    {
+                        try 
+                        {
+                            System.err.print("Transaction is being rolled back");
+                            conn.rollback();
+                        } 
+                        catch(SQLException excep) 
+                        {
+                            System.out.println(excep);
+                        }
+                    }
+                }
+                finally
+                {
+                    try
+                    {
+                        conn.setAutoCommit(true);
+                    }
+                    catch(SQLException excep2)
+                    {
+                        System.out.println(excep2);
+                    }
                 }
             }
+    }
     }
 
     public void doPost(HttpServletRequest request,HttpServletResponse response)	throws ServletException, IOException{
