0be4b13c8dad82fc11eedc90f06bc342f986b644
sagar <sjsagarjha3@gmail.com>
2013-11-14 04:18:28 +0530

    Wrote all the queries that are to be run after an accrediation

--- dbProject/Accredit.java
+++ dbProject/Accredit.java
@@ -26,6 +26,9 @@ public class Accredit extends HttpServlet{
     
     public void handleAccreditionOfMatches (int id, HttpServletRequest request, Connection conn) throws SQLException{
 	System.out.println("In handleAccreditionOfMatches of Accredit.java");
+
+	Statement stmt = conn.createStatement();
+	ResultSet rs;
 	
 	int count = 1;
 
@@ -34,7 +37,140 @@ public class Accredit extends HttpServlet{
 	    count++;
 	    int matchid = Integer.parseInt(value[0]);
 	    if (value[1].equals("2")) {
+		try {
+		    conn.setAutoCommit(false);
 		
+		    String query = "select player1id,player2id,dateofmatch,slotnumber from match where id = " + matchid;
+		    System.out.println(query);
+		    rs = stmt.executeQuery(query);
+		    rs.next();
+		    int winnerID = rs.getInt("player1id"), loserID = rs.getInt("player2id");
+		    String date = rs.getString("dateofmatch");
+		    int slotofmatch = rs.getInt("slotnumber");
+		
+		    query = "Update Match Set Status = 'Completed' where ID = " + matchid;
+		    System.out.println(query);
+		    stmt.execute(query);
+		
+		    query = "Select IDValue from Constant where ConstantName = 'record'";
+		    rs = stmt.executeQuery(query);
+		    rs.next();
+		    int numRecords = rs.getInt("IDValue");
+
+		    query = "select Clb.Name as clubname from Club as Clb, Match as M where M.ID=" + matchid + "and M.ClubID=Clb.ID";
+		    System.out.println(query);
+		    rs = stmt.executeQuery(query);
+		    rs.next();
+		    String clubName = rs.getString("clubname");
+				
+		    query = "select P.Name from Player as P where P.ID=" + winnerID;
+		    System.out.println(query);
+		    rs = stmt.executeQuery(query);
+		    rs.next();
+		    String winnerName = rs.getString("name");
+		
+		    query = "select P.Name from Player as P where P.ID=" + loserID;
+		    System.out.println(query);
+		    rs = stmt.executeQuery(query);
+		    rs.next();
+		    String loserName = rs.getString("name");
+		
+		    String winnerRecord = "Defeated " + loserName + "at " + clubName + " on " + date;
+		    String loserRecord = "Lost to " + winnerName + "at " + clubName + " on " + date;
+		    
+		    query = "Insert into Record values(" + numRecords + ", 'Public', '" + winnerRecord + "', " + winnerID + ")";
+		    System.out.println(query);
+		    stmt.execute(query);
+		    numRecords++;
+
+		    query = "Insert into Record values(" + numRecords + ", 'Public', '" + loserRecord + "', " + loserID + ")";
+		    System.out.println(query);
+		    stmt.execute(query);
+		    numRecords++;
+
+		    query = "Update Constant set IDValue = " + numRecords + " where ConstantName = 'record'";
+		    System.out.println(query);
+		    stmt.execute(query);
+		    
+		    query = "Update Match Set WinnerID =" + winnerID + "where ID = " + matchid;
+		    System.out.println(query);
+		    stmt.execute(query);
+
+		    query = "Update Player Set NumMatchesPlayed = NumMatchesPlayed +1 where ID = " + winnerID;
+		    System.out.println(query);
+		    stmt.execute(query);
+		    
+		    query = "Update Player Set NumMatchesWOn = NumMatchesWon +1 where ID = " + winnerID;
+		    System.out.println(query);
+		    stmt.execute(query);
+		    
+		    query = "Update Player Set NumMatchesPlayed = NumMatchesPlayed +1 where ID =" + loserID;
+		    System.out.println(query);
+		    stmt.execute(query);
+		    
+		    query = "Update Constant Set Slotvalue = case when (Datevalue <= '" + date + "' and  Slotvalue <= " + slotofmatch + ") then " + slotofmatch + " else Slotvalue end where ConstantName = 'time'";
+		    System.out.println(query);
+		    stmt.execute(query);
+		    
+		    query = "Update Constant Set Datevalue = case when Datevalue < " + date + " then " + date + " else Datevalue end where ConstantName = 'time'";
+		    System.out.println(query);
+		    stmt.execute(query);
+		    
+		    String currentDate = "Select Datevalue from Constant where ConstantName = 'time'";
+		    String currentSlot = "Select Slotvalue from Constant where ConstantName = 'time'";
+
+		    query = "Update Match Set Status = 'Accreditation Pending' where Match.DateOfMatch < " + currentDate + "OR ( Match.DateOfMatch = " + currentDate + "AND Match.SlotNumber < " + currentSlot + ") AND Match.status = 'Upcoming'";
+		    System.out.println(query);
+		    stmt.execute(query);
+		    
+		    query = "Delete from Match where Status = 'Challenge' AND ((Match.DateOfMatch < " + currentDate + ") OR ( Match.DateOfMatch = " + currentDate + " AND Match.SlotNumber < " + currentSlot + "))";
+		    System.out.println(query);
+		    stmt.execute(query);
+		    
+		    query = "Select C.TournamentID from Tournament as T, Competitive as  C where C.ID = " + matchid + " AND C.TournamentID = T.ID AND C.Round = log(T.numPlayers)/log(2)";
+		    System.out.println(query);
+		    rs = stmt.executeQuery(query);
+		    while(rs.next()) {
+			int tournamentID = rs.getInt("id");
+			query = "Delete from Outlet where TournamentID = " + tournamentID;
+			System.out.println(query);
+			stmt.execute(query);
+			query = "Update tournament set winnerid = " + winnerID + ", runnerID = " + loserID + " where ID = " + tournamentID;
+			System.out.println(query);
+			stmt.execute(query);
+		    }
+
+		    conn.commit();
+		}
+		
+		catch(SQLException e)
+		    {
+			System.out.println(e);
+			if (conn != null) 
+			    {
+				try 
+				    {
+					System.err.print("Transaction is being rolled back");
+					conn.rollback();
+				    } 
+				catch(SQLException excep) 
+				    {
+					System.out.println(excep);
+				    }
+			    }
+		    }
+		finally
+		    {
+			try
+			    {
+				conn.setAutoCommit(true);
+			    }
+			catch(SQLException excep2)
+			    {
+				System.out.println(excep2);
+			    }
+		    }
+        
 	    }
 	    else if (value[1].equals("3")) {
 		
