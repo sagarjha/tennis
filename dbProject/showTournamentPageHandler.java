9618a0106411370d7902edd552c74eca3b02764c
Rogyan <rohan@ubuntu.ubuntu-domain>
2013-11-15 02:48:55 +0530

    register for tournament by player

--- dbProject/showTournamentPageHandler.java
+++ dbProject/showTournamentPageHandler.java
@@ -22,28 +22,89 @@ public class showTournamentPageHandler extends HttpServlet{
 	Statement stmt1 = conn.createStatement();
 	query = "select name from club where id=" + rs.getString("clubid");
 	System.out.println(query);
-	ResultSet rs1 = stmt.executeQuery(query);
-	rs1.next();
-	request.setAttribute("club",rs1.getString("name"));
+	rs = stmt.executeQuery(query);
+	rs.next();
+	request.setAttribute("club",rs.getString("name"));
         
         int my_id=Integer.parseInt(session.getAttribute("accountid").toString());
         
         //Show register button if player
+        //Checking if it is a player
         query="select * from player where id="+my_id+";";
         rs = stmt.executeQuery(query);
+        System.out.println(query);
         request.setAttribute("registerstall",0);
         if(rs.next())
-            request.setAttribute("registerstall",1);        //1 means register button for player
-        
-        
+        {
+            System.out.println("aaaaaaah");
+            //Checking if the tournament belongs to a club in which the player is a member
+            query="select * from member as m,tournament as t where m.playerid="+my_id+" and t.clubid=m.clubid and t.id="+id+";";
+            rs = stmt.executeQuery(query);
+            System.out.println(query);
+            if(rs.next())
+            {
+                //See if the player is taking part in any tournament (inlcuding this) during the period
+                String start_date="";
+                String end_date="";
+                query="select max(m.dateofmatch) as enddate from match as m, competitive as c where m.id=c.id and m.status='Upcoming' and c.tournamentid="+id+";";
+                rs = stmt.executeQuery(query);
+                System.out.println(query);
+                rs.next();
+                end_date=rs.getString("enddate");
+                System.out.println("Printing end date:"+end_date);
+                query="select startdate from tournament where id="+id+";";
+                rs = stmt.executeQuery(query);
+                System.out.println(query);
+                rs.next();
+                start_date=rs.getString("startdate");
+                
+                
+                //Checking matches which belong in that period
+                query="select * from match where (player1id="+my_id+" or player2id="+my_id+") and (dateofmatch < '"+end_date+"' and dateofmatch > '"+start_date+"');";
+                rs = stmt.executeQuery(query);
+                System.out.println(query);
+                if(rs.next()){}
+                else
+                {
+                    //Check if there is any overlapping tournament in which the player has registered
+                    query="select t.id from plays p, tournament t where p.playerid = "+my_id+" and p.tournamentid = t.id and not ( t.startdate < '"+start_date+"' and (select max(m.dateofmatch) from match m, competitive c where m.id = c.id and c.tournamentid = t.id) <'"+ start_date+"') and not (t.startdate > '"+end_date+"' and (select max(m.dateofmatch) from match m, competitive c where m.id = c.id and c.tournamentid = t.id) > '"+end_date+"');";
+                    rs = stmt.executeQuery(query);
+                    System.out.println(query);
+                    if(rs.next()){}
+                    else
+                    {
+                        //Check if the there are some empty seats for players in the tournament
+                        query="select count(*) as num from plays where tournamentid="+id+";";
+                        rs = stmt.executeQuery(query);
+                        System.out.println(query);
+                        int num_players=rs.getInt("num");
+                        query="select numplayers from tournament where id="+id+";";
+                        System.out.println(query);
+                        rs = stmt.executeQuery(query);
+                        int cap_players=rs.getInt("numplayers");
+                        if((cap_players-num_players)>0)
+                        {
+                            
+                            request.setAttribute("registerstall",1);        //1 means register button for player  
+                        }
+                                              
+                    }
+
+                }
+                
+                
+            }
+        }
         
         //Show add stall button if vendor
         query="select * from vendor where id="+my_id+";";
         rs = stmt.executeQuery(query);
+        System.out.println(query);
         if(rs.next())
         {
             query="Select * from Outlet where vendorID ="+ my_id+" and TournamentID ="+ id+";";
             rs = stmt.executeQuery(query);
+            System.out.println(query);
             if(rs.next()){}
             else
                 request.setAttribute("registerstall",2);        //2 means register button for vendor
