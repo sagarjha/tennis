b703046230ce30cba00496cbfc1763c6f8d0f42d
Rogyan <rohan@ubuntu.ubuntu-domain>
2013-11-14 20:03:57 +0530

    Coaching slot for both player and coach done

--- dbProject/addcoachingslotcoach.java
+++ dbProject/addcoachingslotcoach.java
@@ -7,25 +7,28 @@ import java.io.*;
 
 public class addcoachingslotcoach extends HttpServlet{
     public String addcoachslot(HttpServletRequest request, Connection conn, HttpSession session){
-        int clubid = Integer.parseInt(request.getParameter("newclub").toString());
-        int coachid = Integer.parseInt(session.getAttribute("accountid").toString());
+        int clubid = 0;
+        int coachid = 0;
         String redirectJsp="";        
-        if((request.getParameter("newcoach").toString()=="")|(request.getParameter("newclub").toString()==""))
+        if((request.getParameter("newclub").equals("")))
         {
             redirectJsp="/signupAndLogin/operationnotsuccessful.jsp";
         }
         else
         {
+            clubid = Integer.parseInt(request.getParameter("newclub").toString());
+            coachid = Integer.parseInt(session.getAttribute("accountid").toString());
             redirectJsp="/signupAndLogin/operationsuccessful.jsp";
-        }
-        try{
-            String query = "Insert into teachesat values(" + coachid + ", " + clubid  +");"; 
-            Statement stmt = conn.createStatement();
-            stmt.executeUpdate(query);
-            System.out.println(query);
-        }
-        catch(Exception e){
-            System.out.println(e);
+        
+            try{
+                String query = "Insert into teachesat values(" + coachid + ", " + clubid  +");"; 
+                Statement stmt = conn.createStatement();
+                stmt.executeUpdate(query);
+                System.out.println(query);
+            }
+            catch(Exception e){
+                System.out.println(e);
+            }
         }
         return redirectJsp;
     }
